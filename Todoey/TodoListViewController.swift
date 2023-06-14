d6394ad3e7a8b33e4bdb6f738684bf1f2926ea3a
Shaw Lu <shawlu95@gmail.com>
2023-06-13 19:53:44 -0700

    implement tableview data source

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 }
 
